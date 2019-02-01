44af7d47f0d445aee45f946027bf84b157ec5829
CyberZHG <CyberZHG@gmail.com>
2019-02-01 09:22:53 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,19 +1,27 @@
+import codecs
 from setuptools import setup, find_packages
+
+
+with codecs.open('README.md', 'r', 'utf8') as reader:
+    long_description = reader.read()
+
+
+with codecs.open('requirements.txt', 'r', 'utf8') as reader:
+    install_requires = list(map(lambda x: x.strip(), reader.readlines()))
+
 
 setup(
     name='keras-embed-sim',
-    version='0.1.0',
+    version='0.2.0',
     packages=find_packages(),
     url='https://github.com/CyberZHG/keras-embed-sim',
     license='MIT',
     author='CyberZHG',
     author_email='CyberZHG@gmail.com',
     description='Calculate similarity with embedding',
-    long_description=open('README.rst', 'r').read(),
-    install_requires=[
-        'numpy',
-        'keras',
-    ],
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    install_requires=install_requires,
     classifiers=(
         "Programming Language :: Python :: 2.7",
         "Programming Language :: Python :: 3.6",
