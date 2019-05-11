d3a43da0f44da68dfa094294f8c90f616d5b2ea0
Zhao HG <CyberZHG@gmail.com>
2019-05-11 23:16:05 +0800

    Support tf.keras (#2)

--- tests/test_embeddings.py
+++ tests/test_embeddings.py
@@ -1,9 +1,8 @@
 import os
 import tempfile
-import random
-import keras
 import numpy as np
 from unittest import TestCase
+from keras_embed_sim.backend import keras
 from keras_embed_sim import EmbeddingRet, EmbeddingSim, get_custom_objects
 
 
@@ -20,7 +19,7 @@ class TestEmbeddings(TestCase):
         output_layer = EmbeddingSim(name='Embed-Sim')([embed, embed_weights])
         model = keras.models.Model(inputs=input_layer, outputs=output_layer)
         model.compile(optimizer='adam', loss='mse')
-        model_path = os.path.join(tempfile.gettempdir(), 'test_embed_sim_%f.h5' % random.random())
+        model_path = os.path.join(tempfile.gettempdir(), 'test_embed_sim_%f.h5' % np.random.random())
         model.save(model_path)
         model = keras.models.load_model(model_path, custom_objects=get_custom_objects())
         model.summary(line_length=100)
